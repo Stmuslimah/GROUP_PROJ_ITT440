373c61000a0281f7425dcc29c80180eb9a7cf130
Stmuslimah <muslimahaisyah39@gmail.com>
2021-07-06 10:30:32 +0000

    change code

--- Server.py
+++ Server.py
@@ -14,21 +14,12 @@ while True:
 
 	data , addr = s.recvfrom(1024)
 
-	if data == b'end':
-		False
-		print('\nDetail Record')
-		file = open('Details.txt','r')
-		print(file.read())
-		time.sleep(1)
-		print('Connection ended...\n')
-		time.sleep(1)
-		break
+	print('Connected to ', addr)
 
-	if data != b'end' and  data != b'':
-		print('Connected to ', addr)
-      		
-		file_data = data
-		file.write(str(file_data) + '\n')
+	file_data = data
+	file.write(str(file_data) + '\n')
+	break
+
 
 file.close()
 s.close()
